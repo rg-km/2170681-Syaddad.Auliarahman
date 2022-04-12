2aa13cf741750ec185e31929e7c2747af5da7691
syaddadSmiley <syaddad.snfa@gmail.com>
2022-04-12 21:58:20 +0700

    submit : scoreboard-assignment

--- backend/data-structure/assignment/scoreboard/main.go
+++ backend/data-structure/assignment/scoreboard/main.go
@@ -18,6 +18,18 @@ func (s Scores) Len() int {
 }
 
 func (s Scores) Less(i, j int) bool {
+	if s[i].Correct*4-s[i].Wrong > s[j].Correct*4-s[j].Wrong {
+		return true
+	} else if s[i].Correct*4-s[i].Wrong == s[j].Correct*4-s[j].Wrong {
+		if s[i].Correct > s[j].Correct {
+			return true
+		} else if s[i].Correct == s[j].Correct {
+			if s[i].Name < s[j].Name {
+				return true
+			}
+		}
+	}
+	fmt.Println(s[i].Correct, s[j].Correct)
 	return false // TODO: replace this
 }
 
@@ -37,11 +49,17 @@ func (s Scores) TopStudents() []string {
 func main() {
 	scores := Scores([]Score{
 		{"Levi", 3, 2, 2},
-		{"Agus", 3, 4, 0},
-		{"Doni", 3, 0, 7},
+		{"Agus", 5, 4, 0},
+		{"Doni", 4, 0, 7},
 		{"Ega", 3, 0, 7},
 		{"Anton", 2, 0, 5},
 	})
 	sort.Sort(scores)
+	for i := 0; i < scores.Len()-1; i++ {
+		if scores.Less(i, i+1) {
+			scores.Swap(i, i+1)
+			//fmt.Println()
+		}
+	}
 	fmt.Println(scores.TopStudents())
 }
