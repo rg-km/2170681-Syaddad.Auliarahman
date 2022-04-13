2d9f6ed29fe2a19f34113df78fa3d9292002b2ff
syaddadSmiley <syaddad.snfa@gmail.com>
2022-04-13 08:16:20 +0700

    submit: 1-hashmap-cp

--- backend/data-structure/hashmap/1-hashmap-cp/main.go
+++ backend/data-structure/hashmap/1-hashmap-cp/main.go
@@ -8,15 +8,37 @@ type Person struct {
 }
 
 func main() {
-	people := []Person{{name: "Bob", age: 21}, {name: "Sam", age: 28}, {name: "Ann", age: 21}, {name: "Joe", age: 22}, {name: "Ben", age: 28}}
+	people := []Person{
+		{name: "Bob", age: 21},
+		{name: "Sam", age: 28},
+		{name: "Ann", age: 21},
+		{name: "Joe", age: 22},
+		{name: "Ben", age: 28}}
 	fmt.Println(AgeDistribution(people))
 	fmt.Println(FilterByAge(people, 21))
 }
 
 func AgeDistribution(people []Person) map[int]int {
-	return map[int]int{} // TODO: replace this
+	res := make(map[int]int)
+	for _, age := range people {
+		intAge := int(age.age)
+		if _, ok := res[intAge]; ok {
+			res[intAge] += 1
+		} else {
+			res[intAge] = 1
+		}
+
+	}
+	return res // TODO: replace this
 }
 
 func FilterByAge(people []Person, age int) []Person {
-	return []Person{} // TODO: replace this
+	var result []Person
+	for _, ageMap := range people {
+		intAge := int(ageMap.age)
+		if intAge == age {
+			result = append(result, ageMap)
+		}
+	}
+	return result // TODO: replace this
 }
