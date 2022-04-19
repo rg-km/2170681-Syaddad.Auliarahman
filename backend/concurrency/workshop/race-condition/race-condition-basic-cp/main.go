1ee7a78144f7290894168500a89628d1e1c9ef24
syaddadSmiley <syaddad.snfa@gmail.com>
2022-04-19 16:16:49 +0700

    submit: race-condition-cp

--- backend/concurrency/workshop/race-condition/race-condition-basic-cp/main.go
+++ backend/concurrency/workshop/race-condition/race-condition-basic-cp/main.go
@@ -6,19 +6,29 @@ import "sync"
 func counter(output chan<- int) {
 
 	// TODO: answer here
-
+	c := make(chan int)
 	var wg sync.WaitGroup
 	count := 0
 	for i := 0; i < 1000; i++ {
 		wg.Add(1)
+		//defer wg.Done()
 		go func() {
 			//kirim 1 ke channel
+			defer wg.Done()
+			c <- 1
 			// TODO: answer here
 		}()
 	}
-	//mengubah nilai count menggunakan data dari channel
 
+	//mengubah nilai count menggunakan data dari channel
 	// TODO: answer here
+	go func() {
+		for {
+			//fmt.Println(<-c)
+			count += <-c
+		}
+	}()
 	wg.Wait() // menunggu seluruh goroutine selesai berjalan
 	output <- count
+
 }
