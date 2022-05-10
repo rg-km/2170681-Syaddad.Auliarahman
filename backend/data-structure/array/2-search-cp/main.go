4d93e1584e0912c9273d6011c79413b49c534d3f
syaddadSmiley <syaddad.snfa@gmail.com>
2022-05-10 22:45:18 +0700

    submit: search-cp

--- backend/data-structure/array/2-search-cp/main.go
+++ backend/data-structure/array/2-search-cp/main.go
@@ -15,5 +15,23 @@ func main() {
 }
 
 func SearchMatch(arr1 []string, arr2 []string) ([]string, error) {
-	return []string{}, nil // TODO: replace this
+	// for _, val1 := range arr1{
+	var res []string
+	found := false
+	// }
+	for i := 0; i < len(arr1); i++ {
+		for j := 0; j < len(arr2); j++ {
+			fmt.Println(string(arr1[i]), string(arr2[j]))
+			if arr1[i] == arr2[j] {
+				found = true
+				res = append(res, string(arr1[i]))
+			}
+		}
+	}
+	if found == false {
+		return []string{}, fmt.Errorf("no match")
+	}
+	return res, nil
+
+	// TODO: replace this
 }
