ee56b15f8afaa0d6bc391ceb2e063fa2dc87d630
syaddadSmiley <syaddad.snfa@gmail.com>
2022-06-01 14:10:51 +0700

    submit:range-cp

--- backend/concurrency/workshop/intermediate-channel/1-buffered-channel-1/main.go
+++ backend/concurrency/workshop/intermediate-channel/1-buffered-channel-1/main.go
@@ -3,15 +3,39 @@ package main
 import "fmt"
 
 //buferred channel tidak blocking jika buffer belum penuh
-func main() {
-	input := make(chan string, 3)
+// func test(count int, input chan string) (string, int) {
+// 	var x string
+// 	fmt.Scanf("input :", &x)
+// 	// input <- x
+// 	count++
+// 	return x, count
+// }
 
-	input <- "halo"
-	input <- "dari buffered"
-	input <- "channel"
+func main() {
+	input := make(chan string, 4)
+	// capx := cap(input)
+	// count := 0
+	// for i := 0; i < capx; i++ {
+	// 	if count <= capx {
+	// 		y, count1 := test(count, input)
+	// 		input <- y
+	// 		count = count1
+	// 	}
+
+	// }
+	var arr []string
+	var x string
+	for i := 0; i < cap(input); i++ {
+		fmt.Scanf("Input: ", &x)
+		arr = append(arr, x)
+		input <- arr[i]
+	}
+
+	// input <- "halo"
+	// input <- "dari buffered"
 
 	for i := 0; i < cap(input); i++ {
-		fmt.Println(cap(input))
+		//fmt.Println(cap(input))
 		fmt.Println(<-input)
 	}
 }
