1efab399248952109410c0f593148e0143cbb844
DIANOCAN <syaddad.snfa@gmail.com>
2022-06-15 22:36:35 +0700

    submit: error handling go lang http servber

--- backend/golang-http-server/error-handling/error-handling-cp/main.go
+++ backend/golang-http-server/error-handling/error-handling-cp/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 )
@@ -16,6 +15,25 @@ var err error
 
 var handler http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
 	// TODO: answer here
+	if r.Method != "GET" {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
+	if err != nil {
+		http.Error(w, http.StatusText(500), http.StatusInternalServerError)
+		return
+	}
+	if len(data) == 0 {
+		http.Error(w, http.StatusText(404), http.StatusNotFound)
+		return
+	}
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("X-Content-Type-Options", "nosniff")
+	w.WriteHeader(http.StatusOK)
+	for _, v := range data {
+		w.Write([]byte(v + "\n"))
+	}
+
 }
 
 func main() {
