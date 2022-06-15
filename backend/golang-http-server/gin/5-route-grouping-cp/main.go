d2e9c69f8f9f2da06d824431aaa4a7530d4e2f2f
DIANOCAN <syaddad.snfa@gmail.com>
2022-06-15 23:01:16 +0700

    submit git 4 dan 5

--- backend/golang-http-server/gin/5-route-grouping-cp/main.go
+++ backend/golang-http-server/gin/5-route-grouping-cp/main.go
@@ -27,16 +27,36 @@ var movies = map[int]Movie{
 }
 
 var MovieListHandler = func(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{}) // TODO: replace this
+	// c.JSON(http.StatusOK, gin.H{}) // TODO: replace this
+	c.JSON(http.StatusOK, movies)
 }
 
 var MovieGetHandler = func(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{}) // TODO: replace this
+	// c.JSON(http.StatusOK, gin.H{}) // TODO: replace this
+	id := c.Param("id")
+	idInt, err := strconv.Atoi(id)
+	if err != nil {
+		c.String(http.StatusBadRequest, "Invalid ID")
+		return
+	}
+
+	movie, ok := movies[idInt]
+	if !ok {
+		c.String(http.StatusNotFound, "data not found")
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": movie})
 }
 
 func GetRouter() *gin.Engine {
 	router := gin.Default()
 	// TODO: answer here
+	movie := router.Group("/movie")
+	{
+		movie.GET("/list", MovieListHandler)
+		movie.GET("/get/:id", MovieGetHandler)
+	}
 	return router
 }
 
