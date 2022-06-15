5b3edc5f5eff132321d4e87a3484231b5c62616b
DIANOCAN <syaddad.snfa@gmail.com>
2022-06-15 22:59:30 +0700

    submit: multiplexing-servemuxx-cp

--- backend/golang-http-server/multiplexing/1-servemux-cp/main.go
+++ backend/golang-http-server/multiplexing/1-servemux-cp/main.go
@@ -11,15 +11,31 @@ import (
 // Route kedua yaitu "/hello" yang menghandle SayHelloHandler
 
 func TimeHandler() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {} // TODO: replace this
+	return func(w http.ResponseWriter, r *http.Request) {
+		t := time.Now()
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(fmt.Sprintf("%v, %v %v %v", t.Weekday(), t.Day(), t.Month(), t.Year())))
+	} // TODO: replace this
 }
 
 func SayHelloHandler() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {} // TODO: replace this
+	return func(w http.ResponseWriter, r *http.Request) {
+		param := r.URL.Query().Get("name")
+		var txt string
+		if param == "" {
+			txt = "Hello there"
+		} else {
+			txt = fmt.Sprintf("Hello, %v!", param)
+		}
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(txt))
+	} // TODO: replace this
 }
 
 func GetMux() *http.ServeMux {
 	mux := http.NewServeMux()
+	mux.HandleFunc("/time", TimeHandler())
+	mux.HandleFunc("/hello", SayHelloHandler())
 	// TODO: answer here
 	return mux
 
