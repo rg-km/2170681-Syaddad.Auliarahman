fc6606d3af987037e9a0e21b3666b7fa83e57e30
DIANOCAN <syaddad.snfa@gmail.com>
2022-06-15 22:51:17 +0700

    submit gin

--- backend/golang-http-server/gin/3-path-parameter-cp/main.go
+++ backend/golang-http-server/gin/3-path-parameter-cp/main.go
@@ -33,11 +33,29 @@ var data = map[int]User{
 }
 
 func ProfileHandler() func(c *gin.Context) {
-	return func(c *gin.Context) {} // TODO: replace this
+	// return func(c *gin.Context) {} // TODO: replace this
+	return func(c *gin.Context) {
+		id := c.Param("id")
+		idInt, err := strconv.Atoi(id)
+		if err != nil {
+			c.String(http.StatusBadRequest, "Invalid ID")
+			return
+		}
+		user, ok := data[idInt]
+		if !ok {
+			c.String(http.StatusNotFound, "data not found")
+			return
+		}
+		c.String(http.StatusOK, fmt.Sprintf("Name: %s, Country: %s, Age: %d", user.Name, user.Country, user.Age))
+		fmt.Println(user)
+	}
 }
 
 func GetRouter() *gin.Engine {
-	return &gin.Engine{} // TODO: replace this
+	// return &gin.Engine{} // TODO: replace this
+	router := gin.Default()
+	router.GET("/profile/:id", ProfileHandler())
+	return router
 }
 
 func main() {
