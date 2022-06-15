289c874ce80487638a7292fccb84bcf6d3782aee
DIANOCAN <syaddad.snfa@gmail.com>
2022-06-15 22:57:35 +0700

    submit: response-cp

--- backend/golang-http-server/response-code/response-code-cp/main.go
+++ backend/golang-http-server/response-code/response-code-cp/main.go
@@ -24,12 +24,29 @@ func IsNameExists(name string) bool {
 
 func GetNameHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		// TODO: answer here
+		if r.Method != "GET" {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+		param := r.URL.Query().Get("name")
+
+		if param == "" {
+			http.Error(w, "Name is required", http.StatusBadRequest)
+			return
+		}
+
+		if !IsNameExists(param) {
+			http.Error(w, "Name does not exists", http.StatusNotFound)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		fmt.Println(param)
 	}
 }
 
 func GetMux() *http.ServeMux {
 	mux := http.NewServeMux()
+	mux.HandleFunc("/name", GetNameHandler())
 	// TODO: answer here
 	return mux
 }
