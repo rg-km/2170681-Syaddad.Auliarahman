ba6505918bf988226bef4b1056bec94dcd64f4f7
DIANOCAN <syaddad.snfa@gmail.com>
2022-06-26 19:49:12 +0700

    submit design pattern behavioral

--- backend/design-patterns/behavioral/2-strategy-cp/sortKM/descendingSort.go
+++ backend/design-patterns/behavioral/2-strategy-cp/sortKM/descendingSort.go
@@ -8,4 +8,11 @@ type DescendingSort struct{}
 func (ds *DescendingSort) Sort(array []int) {
 	//choose any sort algo you want
 	// TODO: answer here
+	for i := 0; i < len(array); i++ {
+		for j := i + 1; j < len(array); j++ {
+			if array[i] < array[j] {
+				array[i], array[j] = array[j], array[i]
+			}
+		}
+	}
 }
